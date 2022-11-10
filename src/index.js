8f84bbfa8c68f37e8e5a9a187fe260c7217dd102
Shivamgupta097 <shivam.applicant@gmail.com>
2022-11-10 17:45:25 +0530

    paddng

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';  
 import { BrowserRouter } from 'react-router-dom';
 import CartProvider from './context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/cart-app'>
   <CartProvider>
     <React.StrictMode>
       <App />
@@ -17,7 +16,3 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
